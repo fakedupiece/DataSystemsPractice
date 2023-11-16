b6578e498847b8d03d88e7810dfcafc0d1a753ef
eDuman <eren_duman06@hotmail.com>
2023-11-16 14:25:40 +0300

    general refactoring in shopping site.

--- shop/models.py
+++ shop/models.py
@@ -1,36 +1,64 @@
+# from django.db import models
+#
+
+# class Order(models.Model):
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     shippingAddress = models.OneToOneField("Address", on_delete=models.PROTECT)
+#
+
+# class BillingAddress(models.Model):
+#     customer = models.ManyToManyField("Customer")
+#     address = models.ManyToManyField(Address)
+#
+# class OrderPayment(models.Model):
+#     card_number = models.IntegerField()
+#     txn_id = models.IntegerField()
+#     order = models.ManyToManyField(Order)
+#     billingAddress = models.ManyToManyField(BillingAddress)
+#
+# class OrderItem(models.Model):
+#     order = models.ForeignKey(Order, on_delete=models.PROTECT, null=True, blank=True,
+#                               related_name='order_items', related_query_name='order_item')
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     price = models.IntegerField()
+#     def __str__(self):
+#         return "name: " + self.product.name + ", product ID:" + str(self.product.pk)
+# # Product.item_features = models.ManyToManyField(OrderItem)
+
 from django.db import models
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=2)
+    postcode = models.SmallIntegerField()
+
 class Customer(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+class BillingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
-class Address(models.Model):
-    address = models.TextField()
-
-Order.shippingAddress = models.OneToOneField(Address, on_delete=models.PROTECT)
-
-class BillingAddress(models.Model):
-    customer = models.ManyToManyField(Customer)
-    address = models.ManyToManyField(Address)
+    shippingAddress = models.OneToOneField("Address", on_delete=models.CASCADE)
 
 class OrderPayment(models.Model):
-    card_number = models.IntegerField()
+    card_number = models.BigIntegerField()
     txn_id = models.IntegerField()
-    order = models.ManyToManyField(Order)
-    billingAddress = models.ManyToManyField(BillingAddress)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    billingAddress = models.ForeignKey(BillingAddress, on_delete=models.CASCADE)
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     def __str__(self):
         return "name: " + self.name + ", product ID:" + str(self.pk)
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, null=True, blank=True, related_name='order_items', related_query_name='order_item')
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, null=True, blank=True,
+                              related_name='order_items', related_query_name='order_item')
+    price = models.FloatField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.IntegerField()
     def __str__(self):
         return "name: " + self.product.name + ", product ID:" + str(self.product.pk)
 # Product.item_features = models.ManyToManyField(OrderItem)
-
